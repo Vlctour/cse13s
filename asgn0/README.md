5749411bf2f789cb6ee224072d5d8019a94af615
Vlctor <107158737+Vlctour@users.noreply.github.com>
2022-06-08 17:49:45 -0700

    Add files via upload
    Pushing cse13s files

---
+++ asgn0/README.md
@@ -0,0 +1,15 @@
+# asgn0 - Printing Hello World!
+
+This is a c program that prints out "Hello World!"
+
+## Building
+
+```
+$ clang -Wall -Werror -Wextra -Wpedantic -o hello hello.c
+```
+
+## Running
+
+```
+$ ./hello
+```
---
+++ asgn0/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// This program prints out "Hello World"
+
+int main(void) {
+    // print statement containing "hello world"
+    printf("Hello World!\n");
+    // returns 0 to signify that the code ran all the way through
+    return 0;
+}
